d2f0e481323272b91fd37c5f73272d4e4f4a7b1b
SafiyaElmi <240500598@mycput.ac.za>
2026-05-11 00:14:25 +0200

    Update PaymentFactory.java

--- src/main/java/za/ac/cput/factory/PaymentFactory.java
+++ src/main/java/za/ac/cput/factory/PaymentFactory.java
@@ -8,13 +8,13 @@ PaymentFactory.java
 Payment factory class
 Author: Safiya Elmi
 (240500598)
-Date: 17 March 2026
+Date: 11 May 2026
 */
 
 public class PaymentFactory {
 
     public static Payment createPayment(String paymentRef, double amount, String paymentDate, String paymentMethod,
-                                        String status) {
+                                        String status, String transactionId, String bookingId) {
 
         if (Helper.isNullOrEmpty(paymentRef)) {
             throw new IllegalArgumentException("Payment reference cannot be empty");
@@ -24,16 +24,27 @@ public class PaymentFactory {
             throw new IllegalArgumentException("Payment method cannot be empty");
         }
 
+        if (Helper.isNullOrEmpty(transactionId)) {
+            throw new IllegalArgumentException("Transaction ID cannot be empty");
+        }
+
+        if (Helper.isNullOrEmpty(bookingId)) {
+            throw new IllegalArgumentException("Booking ID cannot be empty");
+        }
+
         if (amount <= 0) {
             throw new IllegalArgumentException("Amount must be greater than 0");
         }
 
         return new Payment.Builder()
                 .setPaymentRef(paymentRef)
-                .setAmount(amount) 
+                .setAmount(amount)
                 .setPaymentDate(paymentDate)
                 .setPaymentMethod(paymentMethod)
                 .setStatus(status)
+                .setTransactionId(transactionId)
+                .setBookingId(bookingId)
+
                 .build();
     }
 }
